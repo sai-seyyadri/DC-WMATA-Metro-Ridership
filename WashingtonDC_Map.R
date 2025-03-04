baaae880864eb93188a520242d4bea7588e99a68
Roshan Aterugu <159937517+RoshanUMD@users.noreply.github.com>
2025-03-03 19:53:45 -0500

    Worked on Map (03-03-2025)

--- WashingtonDC_Map.R
+++ WashingtonDC_Map.R
@@ -1,10 +1,12 @@
 install.packages("sf")
 install.packages("tmap")
+install.packages("tidyverse")
 
 library(sf)
 library(tmap)
+library(tidyverse)
 
-dc_boundary <- st_read("/Users/roshan/Downloads/Washington_DC_Boundary/Washington_DC_Boundary.shp")
+dc_boundary <- st_read("./Washington_DC_Boundary/Washington_DC_Boundary.shp")
 
 stations <- data.frame(
   id = 1:5,
@@ -26,4 +28,87 @@ tm_shape(dc_boundary) +
   tm_bubbles(size = "visitors", col ="blue", alpha = 0.5) +
   tm_borders() +
   tm_basemap("OpenStreetMap")
+
+
+df_dec_04 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - December_04_2024.csv")
+df_dec_21 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - December_21_2024.csv")
+df_jun_05 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - June_05_2024.csv")
+df_jun_15 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - June_15_2024.csv")
+df_mar_06 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - March_6_2024.csv")
+df_mar_23 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - March_23_2024.csv")
+df_sep_04 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - September_4_2024.csv")
+df_sep_21 <- read.csv("./Data/WMATA_Ridership_Summary.xlsx - September_21_2024.csv")
+
+total_dec_04_df <- df_dec_04 %>%
+  # to change of the type of the column to numeric and remove the K
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_dec_21_df <- df_dec_21 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_jun_05_df <- df_jun_05 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_jun_15_df <- df_jun_15 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_mar_06_df <- df_mar_06 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_mar_23_df <- df_mar_23 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_sep_04_df <- df_sep_04 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+total_sep_21_df <- df_sep_21 %>%
+  mutate(Avg.Daily.Entries = as.numeric(gsub("K", "", Avg.Daily.Entries))) %>%
+  group_by(Station.Name) %>%
+  summarise(avg = mean(Avg.Daily.Entries, na.rm = TRUE)*1000)
+
+all_8_day_avg <- total_dec_04_df %>%
+  full_join(total_dec_21_df, by = "Station.Name") %>%
+  full_join(total_jun_05_df, by = "Station.Name") %>%
+  full_join(total_jun_15_df, by = "Station.Name") %>%
+  full_join(total_mar_06_df, by = "Station.Name") %>%
+  full_join(total_mar_23_df, by = "Station.Name") %>%
+  full_join(total_sep_04_df, by = "Station.Name") %>%
+  full_join(total_sep_21_df, by = "Station.Name")
+
+#doesn't work!
+all_8_day_avg_test <- total_dec_04_df %>%
+  full_join(total_dec_21_df, by = "Station.Name") %>%
+  mutate(avg = avg.x + avg.y) %>%
+  select(-avg.x, -avg.y) %>%
+  full_join(total_jun_05_df, by = "Station.Name") %>%
+  mutate(avg = avg + avg.x) %>%
+  select(-avg.x) %>%
+  full_join(total_jun_15_df, by = "Station.Name") %>%
+  mutate(avg = avg + avg.x) %>%
+  select(-avg.x) %>%
+  full_join(total_mar_06_df, by = "Station.Name") %>%
+  mutate(avg = avg + avg.x) %>%
+  select(-avg.x) %>%
+  full_join(total_mar_23_df, by = "Station.Name") %>%
+  mutate(avg = avg + avg.x) %>%
+  select(-avg.x) %>%
+  full_join(total_sep_04_df, by = "Station.Name") %>%
+  mutate(avg = avg + avg.x) %>%
+  select(-avg.x) %>%
+  full_join(total_sep_21_df, by = "Station.Name") %>%
+  mutate(avg = avg + avg.x)
 
